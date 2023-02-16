098e11d673e779706c8de2b2f5f1e321d2270bf2
Basem Mohamed <besoelpop2@gmail.com>
2023-02-16 02:29:52 +0200

    second component finished

--- src/components/packagesSection/packages.jsx
+++ src/components/packagesSection/packages.jsx
@@ -9,10 +9,18 @@ import Data from './data'
 const Packages = (props) => {
 
     const [dropDown, setDropDown] = useState(false)
-
+    const [valueOfEl, setValue] = useState({
+        ifClickTrue: <div className='show-offers btn'> <span>show offers</span><span>&#x25BC;</span> </div>
+        , ifClickFalse: <div className='show-offers btn'> <span>Close offers</span><span>&#x25B2;</span> </div>
+    })
     const dropDownCLick = () => {
-        setDropDown(!false)
+        setDropDown(true)
 
+
+    }
+
+    const menuDisappear = () => {
+        setDropDown(false)
     }
 
     return (
@@ -34,16 +42,18 @@ const Packages = (props) => {
 
                     </div>
                     <div className='show-offers'>
-                        <button onClick={() => {
-                            dropDownCLick()
-                        }} className='btn show-offers'>Show offers</button>
+                        <a href='#packages-menu' onClick={() => {
+                            !dropDown ? dropDownCLick() : menuDisappear()
+                        }} >{
+                                !dropDown ? valueOfEl.ifClickTrue : valueOfEl.ifClickFalse
+                            }</a>
                     </div>
                 </div>
             </div>
-            <div className='trip-packages'>
+            <div id='packages-menu' className={dropDown ? 'active-packages' : 'trip-packages'}>
                 {Data.map((val, id) => {
                     return (
-                        <div className={dropDown ? 'drop-down-animation' : ''}>
+                        <div>
                             <PackageCards key={id} imageMain={val.imageMain} tripName={val.tripName} tripDetails={val.tripDetails} subImg={val.subImg} tripPrice={val.tripPrice} />
                         </div>
                     )
